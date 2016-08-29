d88f09fa1da9404cb8025ce53d23a7cf435ae532
leafunes <funes.s.leandro@gmail.com>
2016-08-28 21:25:31 -0300

    Primer commit

---
+++ src/gui/Gui.java
@@ -0,0 +1,49 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Canvas;
+
+public class Gui {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui window = new Gui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Gui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		Canvas canvas = new Canvas();
+		canvas.setBounds(10, 21, 428, 244);
+		frame.getContentPane().add(canvas);
+	}
+}
