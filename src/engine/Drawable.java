bc0ab6bc4b2c82f38ff6d153458b9bd87d61f4a0
leafunes <funes.s.leandro@gmail.com>
2016-08-28 22:01:46 -0300

    Se agregaron clases basicas

---
+++ src/engine/Drawable.java
@@ -0,0 +1,13 @@
+package engine;
+
+public class Drawable {
+	
+	public int x;
+	public int y;
+
+	public Drawable(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+}
--- src/engine/GameObject.java
+++ src/engine/GameObject.java
@@ -1,5 +1,43 @@
 package engine;
 
-public class GameObject {
+public abstract class GameObject {
+	
+	private int posX;
+	private int posY;
+	private int width;
+	private int height;
+	
+	private Drawable drawable;
+	
+	public GameObject(int x, int y, int width, int height){
+		this.posX = x;
+		this.posY = y;
+		
+		this.width = width;
+		this.height = height;
+		this.drawable = new Drawable(this.posX, this.posY);
+	}
+	
+	public void move(int x, int y, boolean absolute){
+		if(absolute){
+			this.posX = x;
+			this.posY = y;
+		}
+		
+		else{
+			this.posX += x;
+			this.posY += y;
+		}
+		
+	}
+	
+	public Drawable draw(){
+		
+		
+	}
+	
+	public abstract void step();
+	
+	
 
 }
