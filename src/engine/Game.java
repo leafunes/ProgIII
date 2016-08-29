988907a689a3af283ef4c13f4e85e20c0883a009
leafunes <funes.s.leandro@gmail.com>
2016-08-28 22:47:07 -0300

    Implementacion basica del engine

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,5 +1,38 @@
 package engine;
 
+import java.util.ArrayList;
+
 public class Game {
+	
+	private ArrayList<GameRoom> rooms;
+	private int currentRoomPtr;
+	private GameRoom currentRoom;
+	
+	public Game(GameRoom mainRoom){
+		this.currentRoom = mainRoom;
+		this.rooms = new ArrayList<>();
+		this.rooms.add(mainRoom);
+		
+		this.currentRoomPtr = 0;
+	}
+	
+	public void addRoom(GameRoom room){
+		this.rooms.add(room);
+	}
+	
+	public void nextRoom(){
+		this.currentRoomPtr++;
+		
+		if(this.currentRoomPtr >= this.rooms.size())
+			throw new ArrayIndexOutOfBoundsException("No hay siguiente room");
+		
+		this.currentRoom = this.rooms.get(this.currentRoomPtr);
+	}
+
+	
+	public void step(){
+		this.currentRoom.step();
+		
+	}
 
 }
