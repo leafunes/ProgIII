7859771d1d6e6b199c384e1b0e42a8664d8c252f
leafunes <funes.s.leandro@gmail.com>
2016-08-28 22:18:09 -0300

    Implementacion de la clase GameRoom

--- src/engine/GameRoom.java
+++ src/engine/GameRoom.java
@@ -5,16 +5,21 @@ import java.util.ArrayList;
 public class GameRoom {
 	
 	private ArrayList<GameObject> objects;
+	private ArrayList<Drawable> drawables;
 	private int width;
 	private int height;
 	
 	public GameRoom(int width, int height){
 		this.width = width;
 		this.height = height;
+		
+		this.objects = new ArrayList<>();
+		this.drawables = new ArrayList<>();
 	}
 	
 	public void addObject(GameObject obj){
 		this.objects.add(obj);
+		this.drawables.add(obj.getDrawable());
 	}
 	
 	public void step(){
@@ -24,7 +29,7 @@ public class GameRoom {
 	}
 	
 	public ArrayList<Drawable> getDrawables(){
-		
+		return this.drawables;
 	}
 
 }
