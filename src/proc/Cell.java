75fc807bf09d89d5ee04ea21722ebb16ca3a9665
leafunes <funes.s.leandro@gmail.com>
2016-08-31 12:51:31 -0300

    Se arreglaron errores

--- src/proc/Cell.java
+++ src/proc/Cell.java
@@ -1,13 +1,14 @@
 package proc;
 
 import engine.GameObject;
+import engine.Sprite;
 
 public class Cell extends GameObject {
 	
 	private int value;
 
-	public Cell(int x ,int y, int value) {
-		super(x, y, 32, 32);
+	public Cell(int x ,int y, int value, Sprite spr) {
+		super(x, y, 32, 32,spr);
 		
 		this.value = value;
 		
