ee0972acb65067e1ebaa04c02f0b5c936efb142a
leafunes <funes.s.leandro@gmail.com>
2016-08-31 01:43:37 -0300

    Se implemento la clase Sprite

--- src/engine/Sprite.java
+++ src/engine/Sprite.java
@@ -1,14 +1,48 @@
 package engine;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public class Sprite {
 	
-	public Sprite(String route){
+	private ArrayList<BufferedImage> frames;
+	private int length;
+	
+	public Sprite(String folderRoute, int framesLen){
 		
-		BufferedImage a = new BufferedImage(arg0, arg1, arg2)
+		this.frames = new ArrayList<>(framesLen);
 		
+		File file = null;
+		this.length = 0;
 		
+		while(this.length < framesLen){
+			file = new File(folderRoute + "/"+this.length+".png");
+			
+			if(!file.exists())
+				throw new IllegalArgumentException("No se encontro el archivo, " + file.getAbsolutePath());
+			
+			try {
+				final BufferedImage frame = ImageIO.read(file);
+				this.frames.add(frame);
+				this.length++;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+
+	public BufferedImage getFrame(int i) {
+		return this.frames.get(i);
+	}
+
+	public int length() {
+		return this.length;
 	}
 
 }
