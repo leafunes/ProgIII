ba5970059ac969307e2760254c87a95c8b96cbcc
leafunes <funes.s.leandro@gmail.com>
2016-09-09 14:51:43 -0300

    Se agrego la clase falsa Scores

--- data/data/Scores.java
+++ data/data/Scores.java
@@ -1,5 +1,31 @@
 package data;
 
-public class Scores {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
+public class Scores{
+
+	public static ArrayList<Integer> getScores(){
+		ArrayList<Integer> a = new ArrayList<>();
+		
+		Random gen = new Random();
+		
+		for (int i = 0; i < 10; i++) {
+			a.add(gen.nextInt(100000));
+			
+		}
+		
+		Collections.sort(a);
+		
+		Collections.reverse(a);
+		
+		return a;
+		
+	}
+	
+	public static void putScore(int score){
+		
+	}
+	
 }
